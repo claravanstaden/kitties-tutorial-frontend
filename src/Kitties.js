f92e6d286b3d69e07d189e50cd91e2f1e1581177
claravanstaden <Cats 4 life!>
2022-02-04 13:28:36 +0200

    Revert to my version.

--- src/Kitties.js
+++ src/Kitties.js
@@ -6,10 +6,17 @@ import { TxButton } from './substrate-lib/components';
 
 import KittyCards from './KittyCards';
 
+// Construct a Kitty ID from storage key
 const convertToKittyHash = entry =>
   `0x${entry[0].toJSON().slice(-64)}`;
 
-const constructKitty = (hash, { dna, price, gender, owner }) => ({
+// Construct a Kitty object
+const constructKitty = (hash, {
+  dna,
+  price,
+  gender,
+  owner
+}) => ({
   id: hash,
   dna,
   price: price.toJSON(),
@@ -17,41 +24,30 @@ const constructKitty = (hash, { dna, price, gender, owner }) => ({
   owner: owner.toJSON()
 });
 
+// Use React hooks
 export default function Kitties (props) {
-  const { api, keyring } = useSubstrate();
+  const {
+    api,
+    keyring
+  } = useSubstrate();
   const { accountPair } = props;
 
   const [kittyHashes, setKittyHashes] = useState([]);
   const [kitties, setKitties] = useState([]);
   const [status, setStatus] = useState('');
 
+  // Subscription function for setting Kitty IDs
   const subscribeKittyCnt = () => {
     let unsub = null;
 
     const asyncFetch = async () => {
+      // Query KittyCnt from runtime
       unsub = await api.query.substrateKitties.kittyCnt(async cnt => {
-        // Fetch all kitty keys
+        // Fetch all Kitty objects using entries()
         const entries = await api.query.substrateKitties.kitties.entries();
+        // Retrieve only the Kitty ID and set to state
         const hashes = entries.map(convertToKittyHash);
         setKittyHashes(hashes);
-      });
-    };
-
-    asyncFetch();
-
-    return () => {
-      unsub && unsub();
-    };
-  };
-
-  const subscribeKitties = () => {
-    let unsub = null;
-
-    const asyncFetch = async () => {
-      unsub = await api.query.substrateKitties.kitties.multi(kittyHashes, kitties => {
-        const kittyArr = kitties
-          .map((kitty, ind) => constructKitty(kittyHashes[ind], kitty.value));
-        setKitties(kittyArr);
       });
     };
 
@@ -63,8 +59,32 @@ export default function Kitties (props) {
     };
   };
 
-  useEffect(subscribeKitties, [api, kittyHashes]);
-  useEffect(subscribeKittyCnt, [api, keyring]);
+  // Subscription function to construct a Kitty object
+  const subscribeKitties = () => {
+    let unsub = null
+
+    const asyncFetch = async () => {
+      // Get Kitty objects from storage
+      unsub = await api.query.substrateKitties.kitties.multi(kittyHashes, kitties => {
+        // Create an array of Kitty objects from `constructKitty`
+        const kittyArr = kitties.map((kitty, ind) =>
+          constructKitty(kittyHashes[ind], kitty.value)
+        );
+        // Set the array of Kitty objects to state
+        setKitties(kittyArr)
+      });
+    };
+
+    asyncFetch()
+
+    // return the unsubscription cleanup function
+    return () => {
+      unsub && unsub()
+    };
+  };
+
+  useEffect(subscribeKittyCnt, [api, keyring])
+  useEffect(subscribeKitties, [api, kittyHashes])
 
   return <Grid.Column width={16}>
     <h1>Kitties</h1>
